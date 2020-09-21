fb85a3d309c11fe6fc1b8b8070cc44bdbbb94911
gwen1985 <gwen.ellegeest@outlook.com>
2020-09-21 10:14:55 +0200

    initial setup

---
+++ example.php
@@ -0,0 +1,14 @@
+IntelliJ IDEAPhpStormWebStorm
+<?php
+declare(strict_types=1);
+
+require 'classes/Suit.class.php';
+require 'slasses/card.class.php';
+require 'classes/deck.class.php';
+
+$deck = new Deck();
+$deck->shuffle();
+foreach($deck->getCards() AS $card) {
+    echo $card->getUnicodeCharacter(true);
+    echo '<br>';
+}
